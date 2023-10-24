e2b9dd52943c88d055365ba627226a93a915d579
Ammar <ammar.yassr.33@gmail.com>
2023-10-24 06:44:59 +0300

    create company model

---
+++ backend/models/companyModel.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const companySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter your company name"],
+      trim: true,
+      maxLength: [50, "Your companyName cannot exceed 50 characters"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter your email"],
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter your password"],
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Please enter your description"],
+      trim: true,
+    },
+    phone: {
+      type: String,
+      required: [true, "Please enter your phone number"],
+      trim: true,
+    },
+    industry: {
+      type: String,
+      required: [true, "Please enter your industry"],
+      trim: true,
+    },
+    avatar: String,
+    jobs: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Job",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
