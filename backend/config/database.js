5d1c981db68d2a79d5e02ecd30750a4b37126bb4
Ammar <ammar.yassr.33@gmail.com>
2023-10-26 14:36:45 +0300

    commented db error handling

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,14 +1,13 @@
 const mongoose = require("mongoose");
 
 const dbConnection = () => {
-  mongoose
-    .connect(process.env.DB_URI)
-    .then((conn) => {
-      console.log(`Database Connected: ${conn.connection.host}`);
-    })
-    .catch((err) => {
-      console.error(`Database Connection Error: ${err.message}`);
-    });
+  mongoose.connect(process.env.DB_URI).then((conn) => {
+    console.log(`Database Connected: ${conn.connection.host}`);
+  });
+  // .catch((err) => {
+  //   console.error(`Database Connection Error: ${err.message}`);
+  //   // process.exit(1);
+  // });
 };
 
 module.exports = dbConnection;
