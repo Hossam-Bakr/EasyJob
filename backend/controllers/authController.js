80335d28c7126b11b6339e065437c9cfd0a64d22
Ammar <ammar.yassr.33@gmail.com>
2023-11-03 19:30:07 +0200

    Added route protection and role restriction

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,3 +1,4 @@
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 const Company = require("../models/companyModel");
 const generateJWT = require("../utils/generateJWT");
@@ -149,4 +150,84 @@ exports.Login = async (req, res) => {
       message: err.message,
     });
   }
+};
+
+exports.protect = async (req, res, next) => {
+  try {
+    // Getting token and check of it's there
+    let token;
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
+    } else if (req.cookies.jwt) {
+      token = req.cookies.jwt;
+    }
+
+    if (!token)
+      return res.status(401).json({
+        status: httpStatusText.FAIL,
+        message: "You are not logged in! Please log in to get access.",
+      });
+
+    // Verification token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // Check if user(company) still exists
+    const user = await User.findById(decoded.id);
+    const company = await Company.findById(decoded.id);
+
+    if (!user && !company)
+      return res.status(401).json({
+        status: httpStatusText.FAIL,
+        message: "This account does no longer exist.",
+      });
+
+    // Check if user(company) change his password after token created
+    if (user) {
+      if (user.changedPasswordAfter(decoded.iat))
+        return res.status(401).json({
+          status: httpStatusText.FAIL,
+          message: "User recently changed password! Please log in again.",
+        });
+    } else if (company) {
+      if (company.changedPasswordAfter(decoded.iat))
+        return res.status(401).json({
+          status: httpStatusText.FAIL,
+          message: "Company recently changed password! Please log in again.",
+        });
+    }
+
+    // GRANT ACCESS TO PROTECTED ROUTE
+    if (user) {
+      req.user = user;
+    } else if (company) {
+      req.company = company;
+    }
+
+    next();
+  } catch (err) {
+    console.log(err);
+
+    res.status(500).json({
+      status: httpStatusText.ERROR,
+      message: err.message,
+    });
+  }
+};
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (roles.includes("company") && req.company) return next();
+
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({
+        status: httpStatusText.FAIL,
+        message: "You do not have permission to perform this action",
+      });
+    }
+
+    next();
+  };
 };
