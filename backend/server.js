fb930b30bacddb6f7a3248605f6602387ac049e4
Ammar <ammar.yassr.33@gmail.com>
2023-11-07 17:44:58 +0200

    Add unhandled rejection handler to server.js

--- backend/server.js
+++ backend/server.js
@@ -39,3 +39,11 @@ const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
 });
+
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
