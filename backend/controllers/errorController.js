f5a62d90e98689b9e41c0c05e384f1d9960edc7c
Ammar <ammar.yassr.33@gmail.com>
2023-11-07 16:07:41 +0200

    Rename AppError to ApiError

--- backend/controllers/errorController.js
+++ backend/controllers/errorController.js
@@ -1,29 +1,29 @@
-const AppError = require("./../utils/appError");
+const ApiError = require("./../utils/ApiError");
 
 const handleCastErrorDB = (err) => {
   const message = `Invalid ${err.path}: ${err.value}.`;
-  return new AppError(message, 400);
+  return new ApiError(message, 400);
 };
 
 const handleDuplicateFieldsDB = (err) => {
   const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
 
   const message = `Duplicate field value: ${value}. Please use another value!`;
-  return new AppError(message, 400);
+  return new ApiError(message, 400);
 };
 
 const handleValidationErrorDB = (err) => {
   const errors = Object.values(err.errors).map((el) => el.message);
 
   const message = `Invalid input data. ${errors.join(". ")}`;
-  return new AppError(message, 400);
+  return new ApiError(message, 400);
 };
 
 const handleJWTError = () =>
-  new AppError("Invalid token. Please log in again!", 401);
+  new ApiError("Invalid token. Please log in again!", 401);
 
 const handleJWTExpiredError = () =>
-  new AppError("Your token has expired! Please log in again.", 401);
+  new ApiError("Your token has expired! Please log in again.", 401);
 
 const sendErrorDev = (err, req, res) =>
   res.status(err.statusCode).json({
--- backend/utils/ApiError.js
+++ backend/utils/ApiError.js
@@ -1,4 +1,4 @@
-class AppError extends Error {
+class ApiError extends Error {
   constructor(message, statusCode) {
     super(message);
 
@@ -10,4 +10,4 @@ class AppError extends Error {
   }
 }
 
-module.exports = AppError;
+module.exports = ApiError;
