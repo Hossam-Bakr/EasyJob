34a5364bd46b590fc200677cf6ff7562a782dd4a
Ammar <ammar.yassr.33@gmail.com>
2023-11-27 18:26:33 +0200

    Add job routes to mountRoutes function

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,9 @@
 const authRouter = require("./authRoutes");
+const jobRouter = require("./jobRoutes");
 
 const mountRoutes = (app) => {
   app.use("/api/v1/auth", authRouter);
+  app.use("/api/v1/jobs", jobRouter);
 };
 
 module.exports = mountRoutes;
