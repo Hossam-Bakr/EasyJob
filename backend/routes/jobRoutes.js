260d13b6c9f6fefff31ffaa61c49b48b46938358
Ammar <ammar.yassr.33@gmail.com>
2023-12-05 19:55:01 +0200

    Add job validators

--- backend/routes/jobRoutes.js
+++ backend/routes/jobRoutes.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const authController = require("../controllers/authController");
 const jobController = require("../controllers/jobController");
+const {
+  getJobValidator,
+  createJobValidator,
+  updateJobValidator,
+  deleteJobValidator,
+} = require("../utils/validators/jobValidator");
 
 const router = express.Router();
 
@@ -11,20 +17,23 @@ router
   .post(
     authController.protect,
     authController.restrictTo("company"),
+    createJobValidator,
     jobController.createJob
   );
 
 router
   .route("/:id")
-  .get(jobController.getJob)
+  .get(getJobValidator, jobController.getJob)
   .patch(
     authController.protect,
     authController.restrictTo("company"),
+    updateJobValidator,
     jobController.updateJob
   )
   .delete(
     authController.protect,
     authController.restrictTo("company", "admin"),
+    deleteJobValidator,
     jobController.deleteJob
   );
 
