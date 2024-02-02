7798cda939336c7a7a496b4f979f42a87d461d13
Ammar <ammar.yassr.33@gmail.com>
2024-02-02 15:06:26 +0200

    Refactor company model and add companyProfile model

--- backend/models/companyModel.js
+++ backend/models/companyModel.js
@@ -2,72 +2,64 @@ const { DataTypes } = require("sequelize");
 const sequelize = require("../config/database");
 const bcrypt = require("bcryptjs");
 
-const Company = sequelize.define("Company", {
-  id: {
-    type : DataTypes.INTEGER , 
-    primaryKey : true , 
-    autoIncrement : true , 
-    allowNull : false, 
-  } , 
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      len: [3, 50] 
-    }
+const Company = sequelize.define(
+  "Company",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [3, 50],
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.ENUM,
+      values: ["company"],
+      defaultValue: "company",
+    },
   },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-    validate: {
-      isEmail: true 
-    }
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  description: DataTypes.STRING,
-  phone: DataTypes.STRING,
-  industry: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  location: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  founded: DataTypes.DATE,
-  size: {
-    type: DataTypes.ENUM,
-    values: ['1-10', '11-50', '51-200', '201-500', '500+']
-  },
-  avatar: DataTypes.STRING,
-  role: {
-    type: DataTypes.ENUM,
-    values: ['company'],
-    defaultValue: 'company'
+  {
+    timestamps: true,
+    hooks: {
+      beforeSave: async (company) => {
+        if (company.changed("password")) {
+          company.password = await bcrypt.hash(company.password, 12);
+        }
+      },
+    },
   }
-}, {
-  timestamps: true, 
-  hooks: {
-    beforeSave: async (company) => {
-      if (company.changed('password')) {
-        company.password = await bcrypt.hash(company.password, 12);
-      }
-    }
-  }
-});
+);
 
 // instance methods
-Company.prototype.correctPassword = async function(candidatePassword) {
+Company.prototype.correctPassword = async function (candidatePassword) {
   return await bcrypt.compare(candidatePassword, this.password);
 };
 
-Company.prototype.changedPasswordAfter = function(JWTTimestamp) {
+Company.prototype.changedPasswordAfter = function (JWTTimestamp) {
   if (this.passwordChangedAt) {
-    const changedTimestamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10);
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
     return JWTTimestamp < changedTimestamp;
   }
   return false;
