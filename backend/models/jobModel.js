784871d83f35a47c076268167123cd5b8d53865b
Ammar <ammar.yassr.33@gmail.com>
2024-02-03 07:13:46 +0200

    Refactor jobModel.js to include additional fields

--- backend/models/jobModel.js
+++ backend/models/jobModel.js
@@ -1,70 +1,74 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/database");
 
-const Job = sequelize.define("Job", {
-  id: {
-    type : DataTypes.INTEGER , 
-    primaryKey : true , 
-    autoIncrement : true , 
-    allowNull : false, 
-  } , 
-  title: {
-    type: DataTypes.STRING, 
-    allowNull: false,
-    validate: {
-      notEmpty: { msg: "Please enter your job title" }
-    }
+const Job = sequelize.define(
+  "Job",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    requirements: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    workplace: {
+      type: DataTypes.ENUM("remote", "office", "hybrid"),
+      allowNull: false,
+    },
+    salaryRangeMin: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    salaryRangeMax: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    minExperience: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    careerLevel: {
+      type: DataTypes.ENUM(
+        "Student",
+        "Entry",
+        "Junior",
+        "Mid-level",
+        "Experienced/Senior",
+        "Manager/Lead"
+      ),
+      allowNull: false,
+    },
+    type: {
+      type: DataTypes.ENUM(
+        "Full time",
+        "Part time",
+        "Internship",
+        "Volunteering",
+        "Freelance project",
+        "Shift based"
+      ),
+      allowNull: false,
+    },
+    location: {
+      // type: DataTypes.GEOMETRY("POINT"),
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    deadline: DataTypes.DATE,
   },
-  description: {
-    type: DataTypes.TEXT, 
-    allowNull: false,
-    validate: {
-      notEmpty: { msg: "Please enter your description" }
-    }
-  },
-  requirements: {
-    type: DataTypes.STRING, 
-    allowNull: false,
-    validate: {
-      notEmpty: { msg: "Please enter your requirements" }
-    }
-  },
-  salary: {
-    type: DataTypes.FLOAT, 
-    allowNull: false,
-    validate: {
-      notEmpty: { msg: "Please enter your salary" }
-    }
-  },
-  careerLevel: {
-    type: DataTypes.ENUM(
-      "Student", "Entry", "Junior", "Mid-level", "Experienced", "Senior",
-      "Lead", "Manager", "Director", "Executive", "Consultant", "Entrepreneur",
-      "Chief", "Not specified"
-    ),
-    defaultValue: "Not specified"
-  },
-  type: {
-    type: DataTypes.ENUM(
-      "Full time", "Part time", "Internship", "Temporary", "Freelance project",
-      "Work from home", "Other"
-    ),
-    defaultValue: "Other"
-  },
-  location: {
-    // type: DataTypes.GEOMETRY('POINT'), 
-     type: DataTypes.STRING, 
-    allowNull: false
-  },
-  deadline: {
-    type: DataTypes.DATE,
-    allowNull: false,
-    validate: {
-      notEmpty: { msg: "Please enter job deadline" },
-      isDate: { msg: "Deadline must be a valid date" }
-    }
+  {
+    timestamps: true,
   }
-}, {
-  timestamps: true
-});
+);
 module.exports = Job;
