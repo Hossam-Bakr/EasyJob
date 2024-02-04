43854d82d8e9fc08e16a8e169b3bc555fb452079
Ammar <ammar.yassr.33@gmail.com>
2024-02-04 20:15:29 +0200

    Refactor jobController main functions and include related models

--- backend/controllers/jobController.js
+++ backend/controllers/jobController.js
@@ -1,29 +1,113 @@
 const Job = require("../models/jobModel");
+const Company = require("../models/companyModel");
+const Category = require("../models/categoryModel");
+const JobCategory = require("../models/jobCategoryModel");
 const factory = require("./handlerFactory");
+const catchAsync = require("../utils/catchAsync");
 
 exports.getAllJobs = factory.getAll(Job);
-exports.getJob = factory.getOne(Job);
-exports.createJob = factory.createOne(Job);
-exports.updateJob = factory.updateOne(Job);
-exports.deleteJob = factory.deleteOne(Job);
 
+exports.getJob = catchAsync(async (req, res) => {
+  const job = await Job.findByPk(req.params.id, {
+    include: [
+      {
+        model: Company,
+        attributes: ["id", "name", "email", "phone", "industry"],
+      },
+      {
+        model: Category,
+        attributes: ["id", "name"],
+        through: {
+          model: JobCategory,
+          attributes: [],
+        },
+      },
+    ],
+  });
 
-exports.createJobWithCategories = async (req, res) => {
-    console.log(req.body);
-    const { jobData, categoryIds } = req.body; 
-    if (!categoryIds || categoryIds.length < 1 || categoryIds.length > 3) {
-        return res.status(400).json({
-            success: false,
-            message: "You must specify between 1 and 3 categories."
-        });
-    }
-    try {
-        const job = await Job.create(jobData);
-        await job.setCategories(categoryIds);
-        return res.status(201).json({ success: true, jobData , message: "Job created with categories successfully." });
-    } catch (error) {
-        console.error("Error creating job with categories:", error);
-        return res.status(500).json({ success: false, message: "Failed to create job with categories." });
-    }
-};
+  if (!job) {
+    return res.status(404).json({
+      status: "fail",
+      message: "No job found with that ID",
+    });
+  }
 
+  res.status(200).json({
+    status: "success",
+    data: {
+      job,
+    },
+  });
+});
+
+exports.createJob = catchAsync(async (req, res) => {
+  const job = await req.company.createJob(req.body);
+
+  if (!req.body.categoriesId || req.body.categoriesId.length < 1) {
+    return res.status(400).json({
+      success: false,
+      message: "You must specify at least one category.",
+    });
+  }
+
+  await job.setCategories(req.body.categoriesId);
+
+  res.status(201).json({
+    status: "success",
+    data: {
+      job,
+    },
+  });
+});
+
+exports.updateJob = catchAsync(async (req, res) => {
+  const job = await Job.findByPk(req.params.id);
+
+  if (!job) {
+    return res.status(404).json({
+      status: "fail",
+      message: "No job found with that ID",
+    });
+  }
+
+  if (job.CompanyId !== req.company.id) {
+    return res.status(401).json({
+      status: "fail",
+      message: "You are not authorized to update this job",
+    });
+  }
+
+  await job.update(req.body);
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      job,
+    },
+  });
+});
+
+exports.deleteJob = catchAsync(async (req, res) => {
+  const job = await Job.findByPk(req.params.id);
+
+  if (!job) {
+    return res.status(404).json({
+      status: "fail",
+      message: "No job found with that ID",
+    });
+  }
+
+  if (job.CompanyId !== req.company.id) {
+    return res.status(401).json({
+      status: "fail",
+      message: "You are not authorized to delete this job",
+    });
+  }
+
+  await job.destroy();
+
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+});
