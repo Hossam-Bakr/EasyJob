7f5e5ed6e611b4899feae5950ba78e3f1d50e939
Ammar <ammar.yassr.33@gmail.com>
2024-02-04 20:25:09 +0200

    Refactor categoryRoutes to include auth and restrictTo admin

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,11 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const categoryController = require('../controllers/categoryController'); // Adjust the path as necessary
 
-router.get('/', categoryController.getAllCategories);
-router.get('/:id', categoryController.getCategoryById);
-router.post('/', categoryController.createCategory);
-router.put('/:id', categoryController.updateCategory);
-router.delete('/:id', categoryController.deleteCategory);
+const authController = require("../controllers/authController");
+const categoryController = require("../controllers/categoryController");
+
+router.get("/", categoryController.getAllCategories);
+router.get("/:id", categoryController.getCategoryById);
+
+// Protect all routes after this middleware (only admin have access to these routes)
+router.use(authController.protect, authController.restrictTo("admin"));
+
+router.post("/", categoryController.createCategory);
+
+router
+  .route("/:id")
+  .put(categoryController.updateCategory)
+  .delete(categoryController.deleteCategory);
 
 module.exports = router;
