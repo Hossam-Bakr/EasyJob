896b20d2f6aff28ced3b2d38d266912dd6e30d5c
Hossam-Bakr <hossam.b2kr@gmail.com>
2024-02-06 06:21:12 +0200

    refactor candidates controller

--- backend/controllers/candidateController.js
+++ backend/controllers/candidateController.js
@@ -5,7 +5,6 @@ const Category = require("../models/categoryModel");
 const User = require("../models/userModel");
 
 
-
 exports.getCandidatesByCompanyCategories = async (req, res) => {
     const companyId = req.params.companyId;
     try {
@@ -22,21 +21,19 @@ exports.getCandidatesByCompanyCategories = async (req, res) => {
   
       const categoryName = companyCategories.Industry.dataValues.name;
   
-      // Adjusted query to include related User information
       const userProfiles = await UserProfile.findAll({
         where: sequelize.literal(`JSON_CONTAINS(jobCategories->"$.name", '"${categoryName}"')`),
         include: [{
           model: User,
-          as: 'User', // Ensure this alias matches how you've defined the relationship
-          attributes: ['id', 'firstName', 'lastName', 'email', 'role'] // Specify the attributes you want to include from the User model
+          as: 'User',
+          attributes: ['id', 'firstName', 'lastName', 'email', 'role'] 
         }]
       });
   
-      // Map over userProfiles to format the response if needed
       const formattedProfiles = userProfiles.map(profile => {
         return {
-          userProfile: profile.dataValues, // Contains UserProfile data
-          user: profile.User // Contains associated User data
+          userProfile: profile.dataValues,
+          user: profile.User 
         };
       });
   
@@ -52,153 +49,4 @@ exports.getCandidatesByCompanyCategories = async (req, res) => {
       });
     }
   };
-  
-
-
-
-
-
-
-
-
-
-
-// exports.getCandidatesByCompanyCategories = async (req, res) => {
-//     const companyId = req.params.companyId;
-//     try {
-//       const companyCategories = await Company.findByPk(companyId, {
-//         include: [{
-//           model: Category,
-//           as: "Industry",
-//         }],
-//       });
-  
-//       if (!companyCategories || !companyCategories.Industry) {
-//         return res.status(404).json({ message: "Company or company categories not found" });
-//       }
-      
-//       const categoryName = companyCategories.Industry.dataValues.name;
-  
-//       // Query to find user profiles where 'name' JSON array contains 'categoryName'
-//       const userProfiles = await UserProfile.findAll({
-//         where: sequelize.literal(`JSON_CONTAINS(jobCategories->"$.name", '"${categoryName}"')`)
-//       });
-  
-//       const userProfileData = userProfiles.map(profile => profile.dataValues);
-  
-//       res.json({
-//         status: "success",
-//         userProfiles: userProfileData,
-//       });
-//     } catch (error) {
-//       console.error("Error fetching candidates:", error);
-//       res.status(500).json({
-//         status: "error",
-//         message: "Failed to fetch candidates.",
-//       });
-//     }
-//   };
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// exports.getCandidatesByCompanyCategories = async (req, res) => {
-//     const companyId = req.params.companyId;
-//     try {
-//       const companyCategories = await Company.findByPk(companyId, {
-//         include: [{
-//           model: Category,
-//           as: "Industry",
-//         }],
-//       });
-  
-//       if (!companyCategories || !companyCategories.Industry) {
-//         return res.status(404).json({ message: "Company or company categories not found" });
-//       }
-      
-//       const categoryName = companyCategories.Industry.dataValues.name;
-  
-//       // Adjusted query to correctly access JSON data
-//       const userProfiles = await UserProfile.findAll({
-//         where: sequelize.literal(`jobCategories->"$.name" = '${categoryName}'`)
-//       });
-  
-//       const userProfileData = userProfiles.map(profile => profile.dataValues);
-  
-//       res.json({
-//         status: "success",
-//         userProfiles: userProfileData,
-//       });
-//     } catch (error) {
-//       console.error("Error fetching candidates:", error);
-//       res.status(500).json({
-//         status: "error",
-//         message: "Failed to fetch candidates.",
-//       });
-//     }
-//   };
-  
-
-
-//   exports.getCandidatesByCompanyCategories = async (req, res) => {
-//     const companyId = req.params.companyId;
-//     try {
-//       const companyCategories = await Company.findByPk(companyId, {
-//         include: [
-//           {
-//             model: Category,
-//             as: "Industry",
-//           },
-//         ],
-//       });
-  
-//       if (!companyCategories || !companyCategories.Industry) {
-//         return res
-//           .status(404)
-//           .json({ message: "Company or company categories not found" });
-//       }
-      
-//       const categoryName = companyCategories.Industry.dataValues.name;
-  
-//       const userProfiles = await UserProfile.findAll({
-//           where: {
-//               jobCategories: categoryName 
-//           }
-//       });
-  
-//       const userProfileData = userProfiles.map(profile => profile.dataValues);
-  
-//       res.json({
-//         status: "success",
-//         userProfiles: userProfileData 
-//       });
-  
-//     } catch (error) {
-//       console.error("Error fetching candidates:", error);
-//       res.status(500).json({
-//         status: "error",
-//         message: "Failed to fetch candidates.",
-//       });
-//     }
-//   };
   
