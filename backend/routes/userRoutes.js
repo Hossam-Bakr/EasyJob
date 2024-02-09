5d3ad6e90af685fdc1f08a4b0306b4f8b55f6378
Ammar <ammar.yassr.33@gmail.com>
2024-02-09 14:50:01 +0200

    Add user validators for profile updates

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const userController = require("../controllers/userController");
 const authController = require("../controllers/authController");
-// const userValidator = require("../utils/validators/userValidator");
+const userValidator = require("../utils/validators/userValidator");
 
 router.use(authController.protect, authController.restrictTo("user"));
 
@@ -15,92 +15,84 @@ router.patch(
 );
 router.patch(
   "/profile/info",
-  // userValidator.updateUserInfoValidator,
+  userValidator.updateUserInfoValidator,
   userController.updateUserInfo
 );
 router.patch(
   "/profile/interests",
-  // userValidator.updateUserCareerInterestsValidator,
+  userValidator.updateUserCareerInterestsValidator,
   userController.updateUserCareerInterests
 );
 
 // Experience
 router.patch(
   "/profile/experience/total",
-  // userValidator.updateTotalExperienceValidator,
+  userValidator.updateTotalExperienceValidator,
   userController.updateTotalExperience
 );
-router.route("/profile/experience").post(
-  // userValidator.addExperienceValidator,
-  userController.addExperience
-);
+router
+  .route("/profile/experience")
+  .post(userValidator.addExperienceValidator, userController.addExperience);
 router
   .route("/profile/experience/:id")
   .patch(
-    // userValidator.updateExperienceValidator,
+    userValidator.updateExperienceValidator,
     userController.updateExperience
   )
   .delete(
-    // userValidator.deleteExperienceValidator,
+    userValidator.deleteExperienceValidator,
     userController.deleteExperience
   );
 
 // Education
-router.route("/profile/education/level").patch(
-  // userValidator.updateEducationLevelValidator,
-  userController.updateEducationLevel
-);
-router.route("/profile/education").post(
-  // userValidator.addEducationValidator,
-  userController.addEducation
-);
+router
+  .route("/profile/education/level")
+  .patch(
+    userValidator.updateEducationLevelValidator,
+    userController.updateEducationLevel
+  );
+router
+  .route("/profile/education")
+  .post(userValidator.addEducationValidator, userController.addEducation);
 router
   .route("/profile/education/:id")
-  .patch(
-    // userValidator.updateEducationValidator,
-    userController.updateEducation
-  )
+  .patch(userValidator.updateEducationValidator, userController.updateEducation)
   .delete(
-    // userValidator.deleteEducationValidator,
+    userValidator.deleteEducationValidator,
     userController.deleteEducation
   );
 
 // Certification
-router.route("/profile/certification").post(
-  // userValidator.addCertificationValidator,
-  userController.addCertification
-);
+router
+  .route("/profile/certification")
+  .post(
+    userValidator.addCertificationValidator,
+    userController.addCertification
+  );
 router
   .route("/profile/certification/:id")
   .patch(
-    // userValidator.updateCertificationValidator,
+    userValidator.updateCertificationValidator,
     userController.updateCertification
   )
   .delete(
-    // userValidator.deleteCertificationValidator,
+    userValidator.deleteCertificationValidator,
     userController.deleteCertification
   );
 
 // Language
-router.route("/profile/language").post(
-  // userValidator.addLanguageValidator,
-  userController.addLanguage
-);
+router
+  .route("/profile/language")
+  .post(userValidator.addLanguageValidator, userController.addLanguage);
 router
   .route("/profile/language/:id")
-  .patch(
-    // userValidator.updateLanguageValidator,
-    userController.updateLanguage
-  )
-  .delete(
-    // userValidator.deleteLanguageValidator,
-    userController.deleteLanguage
-  );
+  .patch(userValidator.updateLanguageValidator, userController.updateLanguage)
+  .delete(userValidator.deleteLanguageValidator, userController.deleteLanguage);
 
 // Online Presence
 router.patch(
   "/profile/online-presence",
-  // userValidator.updateOnlinePresenceValidator,
+  userValidator.updateOnlinePresenceValidator,
   userController.updateOnlinePresence
 );
 
