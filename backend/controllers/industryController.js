1ff1fd09a3558f035e7f32bd1bfaa2a77ee6ea1f
Ammar <ammar.yassr.33@gmail.com>
2024-02-22 14:10:29 +0200

    Add  include categories in getAllIndustries

--- backend/controllers/industryController.js
+++ backend/controllers/industryController.js
@@ -1,8 +1,14 @@
 const Industry = require("../models/industryModel");
+const Category = require("../models/categoryModel");
 const factory = require("./handlerFactory");
 
+const includeCategoryObj = {
+  model: Category,
+  attributes: ["id", "name"],
+};
+
 exports.createIndustry = factory.createOne(Industry);
-exports.getAllIndustries = factory.getAll(Industry);
+exports.getAllIndustries = factory.getAll(Industry, includeCategoryObj);
 exports.getIndustry = factory.getOne(Industry);
 exports.updateIndustry = factory.updateOne(Industry);
 exports.deleteIndustry = factory.deleteOne(Industry);
