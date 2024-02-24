76d66422037923087572697d178fbc10fb224c0d
Ammar <ammar.yassr.33@gmail.com>
2024-02-24 14:40:44 +0200

    Update  job routes

--- backend/routes/jobRoutes.js
+++ backend/routes/jobRoutes.js
@@ -3,38 +3,35 @@ const express = require("express");
 const authController = require("../controllers/authController");
 const jobController = require("../controllers/jobController");
 const {
-  getJobValidator,
   createJobValidator,
   updateJobValidator,
-  deleteJobValidator,
 } = require("../utils/validators/jobValidator");
 
 const router = express.Router();
 
-router.route("/").get(jobController.getAllJobs).post(
-  authController.protect,
-  authController.restrictTo("company"),
-  // createJobValidator,
-  jobController.createJob
-);
+router
+  .route("/")
+  .get(jobController.getAllJobs)
+  .post(
+    authController.protect,
+    authController.restrictTo("company"),
+    createJobValidator,
+    jobController.createJob
+  );
 router.get("/latest/:limit", jobController.getLatestJob);
 
 router
   .route("/:id")
-  .get(
-    // getJobValidator,
-    jobController.getJob
-  )
+  .get(jobController.getJob)
   .delete(
     authController.protect,
     authController.restrictTo("company", "admin"),
-    // deleteJobValidator,
     jobController.deleteJob
   )
   .patch(
     authController.protect,
     authController.restrictTo("company"),
-    // updateJobValidator,
+    updateJobValidator,
     jobController.updateJob
   );
 
