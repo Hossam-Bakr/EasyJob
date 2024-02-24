f5ce4af4ce8ebfd220f0029330cad41e55cb0987
BassamHafez <bassamhafez790@gmail.com>
2024-02-24 18:32:55 +0200

    .

--- frontend/src/Components/Ui/FloatingPopup.jsx
+++ frontend/src/Components/Ui/FloatingPopup.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./FloatingPopup.module.css";
 import Toast from "react-bootstrap/Toast";
 import ToastContainer from "react-bootstrap/ToastContainer";
@@ -10,19 +10,37 @@ import {
 
 const FloatingPopup = ({ showResponse, setShowResponse, message, success }) => {
 
-  const toastHeaderClasses = success ? styles.popupHeaderSuccess : styles.popupHeaderFaild;
+  const [toastVisible, setToastVisible] = useState(false);
 
+  useEffect(() => {
+    setToastVisible(false);
+  }, []);
+
+  useEffect(() => {
+    setToastVisible(showResponse);
+  }, [showResponse]); 
+
+
+  const toastHeaderClasses = success
+  ? styles.popupHeaderSuccess
+  : styles.popupHeaderFaild;
+
+  const toastContainer = toastVisible
+  ? styles.popup_container
+  : styles.popup_container_hidden;
+
+ 
   return (
     <>
       <div
         aria-live="assertive"
         aria-atomic="true"
-        className={styles.popup_container}
+        className={toastContainer}
       >
         <ToastContainer style={{ zIndex: 1 }}>
           <Toast
             onClose={() => setShowResponse(false)}
-            show={showResponse}
+            show={toastVisible}
             delay={3000}
             autohide
             className={styles.floating_popup}
@@ -44,9 +62,7 @@ const FloatingPopup = ({ showResponse, setShowResponse, message, success }) => {
               </strong>
               <small>Now</small>
             </Toast.Header>
-            <Toast.Body >
-              {message?.content}
-            </Toast.Body>
+            <Toast.Body>{message?.content}</Toast.Body>
           </Toast>
         </ToastContainer>
       </div>
