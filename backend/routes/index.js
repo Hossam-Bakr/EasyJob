2fd570e95ef15505fab4662556ccc529248b9d93
Ammar <ammar.yassr.33@gmail.com>
2024-06-22 15:20:28 +0300

    Add pricing plan and subscription routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,6 +7,8 @@ const skillRouter = require("./skillRoutes");
 const companyRouter = require("./companyRoutes");
 const userRouter = require("./userRoutes");
 const specificTasksRouter = require("./specificTasksRoutes");
+const pricingPlanRouter = require("./pricingPlanRoutes");
+const subscriptionRouter = require("./subscriptionRoutes");
 
 const mountRoutes = (app) => {
   app.use("/api/v1/auth", authRouter);
@@ -18,6 +20,8 @@ const mountRoutes = (app) => {
   app.use("/api/v1/users", userRouter);
   app.use("/api/v1/companies", companyRouter);
   app.use("/api/v1/specificTasks", specificTasksRouter);
+  app.use("/api/v1/plans", pricingPlanRouter);
+  app.use("/api/v1/subscriptions", subscriptionRouter);
 };
 
 module.exports = mountRoutes;
